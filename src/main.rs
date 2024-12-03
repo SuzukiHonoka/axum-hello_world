00000512c981bf97e1b4e4e23250c23142de4158
Suzukihonoka <sakurafirex@gmail.com>
2024-12-03 16:44:48 +0800

    refactor: minor improvements

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::process;
 use axum::{
     routing::get,
     Router,
 };
+use std::process;
+use tokio::signal;
 use tracing::info;
 use tracing_subscriber;
 
@@ -10,32 +11,21 @@ const BIND_ADDR: &str = "0.0.0.0:8080";
 
 #[tokio::main]
 async fn main() {
-    // start a thread for handle signal
-    tokio::spawn(async move {
-        handle_signal().await
-    });
-
     // init tracing
     tracing_subscriber::fmt::init();
 
     // build app with a router
     let app = Router::new().route("/", get(root));
 
-    let listener = match tokio::net::TcpListener::bind(BIND_ADDR).await {
-        Ok(listener) => listener,
-        Err(e) => {
-            eprintln!("Failed to bind to addr {}: {}", BIND_ADDR, e);
-            return;
-        }
-    };
+    let listener = tokio::net::TcpListener::bind(BIND_ADDR).await.unwrap_or_else(|e| {
+        eprintln!("Failed to bind to addr {}: {}", BIND_ADDR, e);
+        process::exit(1);
+    });
 
     println!("server will starts listening on {}", BIND_ADDR);
-    match axum::serve(listener, app).await {
-        Ok(_) => (),
-        Err(e) => {
-            eprintln!("Failed to serve: {}", e);
-        }
-    };
+    if let Err(e) = axum::serve(listener, app).with_graceful_shutdown(handle_signal()).await {
+        eprintln!("Failed to serve: {}", e);
+    }
 }
 
 async fn root() -> &'static str {
@@ -44,23 +34,25 @@ async fn root() -> &'static str {
 }
 
 async fn handle_signal() {
-    #[cfg(target_os = "windows")]
-    {
-        use tokio::signal::windows::ctrl_c;
-        let mut signal = ctrl_c().expect("Failed to listen for signal");
-        signal.recv().await;
-    }
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
 
-    #[cfg(not(target_os = "windows"))]
-    {
-        use tokio::signal::unix::{signal, SignalKind};
-        let mut sigint = signal(SignalKind::interrupt()).expect("failed to create SIGINT signal stream");
-        let mut sigterm = signal(SignalKind::terminate()).expect("failed to create SIGTERM signal stream");
-        tokio::select! {
-            _ = sigint.recv().await => (),
-            _ = sigterm.recv().await => ()
-        }
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
-    println!("Received signal, shutting down");
-    process::exit(0);
 }
